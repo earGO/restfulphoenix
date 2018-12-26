8ac8c158c16d3fabc7f17894a6491f66360ddca7
earGO <rh.edwin@gmail.com>
2018-12-26 07:41:17 -0600

    added index an index-route

--- app.js
+++ app.js
@@ -2,23 +2,47 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require("mongoose"),
     app = express(),
-    port = 3000;
+    port = 3100;
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect('', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/phoenix_realty_laws', {useNewUrlParser: true})
 
-var NEW_Schema = new mongoose.Schema({
-    //schema goes here
+var postSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    created: {type: Date,default: Date.now()},
+    body: String,
+    link: String
 })
 
-var DBNAME = mongoose.model('', NEW_Schema);
+var RealtyLaw = mongoose.model('RealtyLaw', postSchema);
 
 app.get('/', (req, res) => {
-    res.send('got server up and running')
+    res.redirect('/phoenix')
 })
+
+//RESTful routes are
+//INDEX - get - /phoenix - shows all blog posts in a short versions
+app.get('/phoenix',(req,res)=>{
+    RealtyLaw.find({},(err,posts)=>{
+        if(err){
+            console.log('got error fetching posts: \n',err)
+        } else {
+            console.log('successfully fetched posts')
+            res.render('index',{posts:posts})
+        }
+    })
+})
+
+//NEW - get - /phoenix/new - shows a form for a new blog post
+//CREATE - post - /phoenix - creates new blog post, records it in a database and then redirects somewhere
+//SHOW - get - /phoenix/:id - shows full single post
+//EDIT - get - /phoenix/:id/edit - shows edit form for a single post
+//UPDATE - put - /phoenix - updates a content of the post in a database then redirects somewhere
+//DESTROY - delete - /phoenix/:id - deletes a post from a database
 
 app.listen(port, function () {
     console.log('server up and running on port', port)
